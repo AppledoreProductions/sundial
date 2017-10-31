8a3ecf2d3ae50e7069a7b7eacbfba85561f9c6c0
AppledoreProductions <appledore.productions@gmail.com>
2017-10-31 15:32:14 +0000

    lineset

--- SundialDirectives.js
+++ SundialDirectives.js
@@ -10,24 +10,24 @@ sdDir.drawGeoGlobeDirective = function () {
 	// grey background circle
 	template += '<ellipse cx="{{globe.centre}}" cy="{{globe.centre}}" rx="{{globe.radius}}" ry="{{globe.radius}}" style="fill:dimgrey;fill-opacity:100%" ></ellipse>';
 	// top left yellow
-	template += '<ellipse cx="{{otherLineSet.topleft.cx}}" cy="{{otherLineSet.topleft.cy}}" rx="{{otherLineSet.topleft.rx}}" ry="{{otherLineSet.topleft.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#top-left)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.topleft.cx}}" cy="{{lineSet.topleft.cy}}" rx="{{lineSet.topleft.rx}}" ry="{{lineSet.topleft.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#top-left)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// top left grey
-	template += '<ellipse cx="{{otherLineSet.topleftblock.cx}}" cy="{{otherLineSet.topleftblock.cy}}" rx="{{otherLineSet.topleftblock.rx}}" ry="{{otherLineSet.topleftblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#top-left)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.topleftblock.cx}}" cy="{{lineSet.topleftblock.cy}}" rx="{{lineSet.topleftblock.rx}}" ry="{{lineSet.topleftblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#top-left)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// top right yellow
-	template += '<ellipse cx="{{otherLineSet.topright.cx}}" cy="{{otherLineSet.topright.cy}}" rx="{{otherLineSet.topright.rx}}" ry="{{otherLineSet.topright.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#top-right)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.topright.cx}}" cy="{{lineSet.topright.cy}}" rx="{{lineSet.topright.rx}}" ry="{{lineSet.topright.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#top-right)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// top right grey
-	template += '<ellipse cx="{{otherLineSet.toprightblock.cx}}" cy="{{otherLineSet.toprightblock.cy}}" rx="{{otherLineSet.toprightblock.rx}}" ry="{{otherLineSet.toprightblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#top-right)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.toprightblock.cx}}" cy="{{lineSet.toprightblock.cy}}" rx="{{lineSet.toprightblock.rx}}" ry="{{lineSet.toprightblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#top-right)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// bottom left yellow
-	template += '<ellipse cx="{{otherLineSet.left.cx}}" cy="{{otherLineSet.left.cy}}" rx="{{otherLineSet.left.rx}}" ry="{{otherLineSet.left.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#bottom-left)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.left.cx}}" cy="{{lineSet.left.cy}}" rx="{{lineSet.left.rx}}" ry="{{lineSet.left.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#bottom-left)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// bottom left grey
-	template += '<ellipse cx="{{otherLineSet.leftblock.cx}}" cy="{{otherLineSet.leftblock.cy}}" rx="{{otherLineSet.leftblock.rx}}" ry="{{otherLineSet.leftblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#bottom-left)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.leftblock.cx}}" cy="{{lineSet.leftblock.cy}}" rx="{{lineSet.leftblock.rx}}" ry="{{lineSet.leftblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#bottom-left)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// bottom right yellow
-	template += '<ellipse cx="{{otherLineSet.right.cx}}" cy="{{otherLineSet.right.cy}}" rx="{{otherLineSet.right.rx}}" ry="{{otherLineSet.right.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#bottom-right)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<ellipse cx="{{lineSet.right.cx}}" cy="{{lineSet.right.cy}}" rx="{{lineSet.right.rx}}" ry="{{lineSet.right.ry}}" style="fill:lemonchiffon;fill-opacity:100%" clip-path="url(#bottom-right)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
 	// bottom right grey
-	template += '<ellipse cx="{{otherLineSet.rightblock.cx}}" cy="{{otherLineSet.rightblock.cy}}" rx="{{otherLineSet.rightblock.rx}}" ry="{{otherLineSet.rightblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#bottom-right)" transform="rotate({{otherLineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
-	template += '<line ng-repeat="line in otherLineSet.latLines" x1="{{line.x1}}" x2="{{line.x2}}" y1="{{line.y1}}" y2="{{line.y2}}" style="{{line.style}}"></line>';
-	template += '<ellipse ng-repeat="ring in otherLineSet.lonRings" cx="{{ring.cx}}" cy="{{ring.cy}}" rx="{{ring.rx}}" ry="{{ring.ry}}" style="{{ring.style}}" clip-path="url(#{{ring.meridian}})" ></ellipse>';
-	template += '<rect x="{{otherLocationBox.x}}" y="{{otherLocationBox.y}}" width="{{otherLocationBox.width}}" height="{{otherLocationBox.height}}" style="{{otherLocationBox.style}}" ></rect>';
+	template += '<ellipse cx="{{lineSet.rightblock.cx}}" cy="{{lineSet.rightblock.cy}}" rx="{{lineSet.rightblock.rx}}" ry="{{lineSet.rightblock.ry}}" style="fill:dimgrey;fill-opacity:100%" clip-path="url(#bottom-right)" transform="rotate({{lineSet.suntilt}} {{globe.centre}} {{globe.centre}})" ></ellipse>';
+	template += '<line ng-repeat="line in lineSet.latLines" x1="{{line.x1}}" x2="{{line.x2}}" y1="{{line.y1}}" y2="{{line.y2}}" style="{{line.style}}"></line>';
+	template += '<ellipse ng-repeat="ring in lineSet.lonRings" cx="{{ring.cx}}" cy="{{ring.cy}}" rx="{{ring.rx}}" ry="{{ring.ry}}" style="{{ring.style}}" clip-path="url(#{{ring.meridian}})" ></ellipse>';
+	template += '<rect x="{{locationBox.x}}" y="{{locationBox.y}}" width="{{locationBox.width}}" height="{{locationBox.height}}" style="{{locationBox.style}}" ></rect>';
 	template += '</svg>';
 	return { template: template	};
 };
